b21b5a7fa5c8ce44a2190e00b85d3131d8f70e80
Dushyant <117421735+Honey25609@users.noreply.github.com>
2024-06-24 17:45:11 +0530

    done

--- BattleDex/src/main.jsx
+++ BattleDex/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
     <SpeedInsights />
+    <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )

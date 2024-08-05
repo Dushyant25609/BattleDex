3d38e3730627da2f34d0ffb0fa270f635d42c4c5
Dushyant <117421735+Honey25609@users.noreply.github.com>
2024-08-05 07:37:52 +0530

    scrollable

--- BattleDex/src/App.jsx
+++ BattleDex/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   }
 
   return (
-    <div className='overflow-hidden'>
+    <div className=''>
       <Background type={type} />
       <Routes>
         <Route path='/' element={<Home/>}/>
